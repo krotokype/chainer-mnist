e1fc33e087c3071936ab9673f612dad978ab9768
krotokype <36023293+krotokype@users.noreply.github.com>
2018-02-03 00:40:20 +0900

    Move model class

--- train.py
+++ train.py
@@ -7,22 +7,9 @@ import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
 
+from model import TLP
 from sys import argv
 from os import path
-
-
-class TLP(Chain):  # Two Layer Perceptron
-
-    def __init__(self, n_units, n_out):
-        super(TLP, self).__init__()
-        with self.init_scope():
-            self.l1 = L.Linear(None, n_units)  # n_in -> n_units
-            self.l2 = L.Linear(None, n_out)  # n_units -> n_out
-
-    def __call__(self, x):
-        h1 = F.sigmoid(self.l1(x))
-        return self.l2(h1)
-
 
 # Set up parameters
 unit = 50
